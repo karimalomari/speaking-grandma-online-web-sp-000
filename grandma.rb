d7938927f29a248242c6a07353677bd0f0f648d0
Karim Alomari <karim.alomari@gmail.com>
2020-07-22 17:34:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,8 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  puts "HUH?! SPEAK UP, SONNY!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
 end
 
 
