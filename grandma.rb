37da4d03872a5e4b2bdbb96c117065d42b33f121
Karim Alomari <karim.alomari@gmail.com>
2020-07-22 17:32:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,8 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma
+def speak_to_grandma(phrase)
+  
   puts "I LOVE YOU TOO PUMPKIN!"
   puts "HUH?! SPEAK UP, SONNY!"
   puts "NO, NOT SINCE 1938!"
